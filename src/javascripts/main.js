52358b128e5a9f554fa4b45e81c2b11d6f1f9fc1
vincentniyitanga <89998405+vincentniyitanga@users.noreply.github.com>
2021-11-27 18:34:09 +0200

    Initial commit

---
+++ src/javascripts/main.js
@@ -0,0 +1,8 @@
+// Required by Webpack - do not touch
+require.context('../', true, /\.(html|json|txt|dat)$/i)
+require.context('../images/', true, /\.(gif|jpg|png|svg|eot|ttf|woff|woff2)$/i)
+require.context('../stylesheets/', true, /\.(css)$/i)
+
+//TODO - Your ES6 JavaScript code (if any) goes here
+import 'bootstrap'
+
